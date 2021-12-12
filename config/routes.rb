bc861c93bd8914614eef6ae8f22467996d5393c8
kenji hajiro <mademoiselle3824@gmail.com>
2021-12-12 15:03:13 +0900

    初期設定

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :pictures do
+    
+  end
+  resources :users do
+
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
