37a0ad502f0c54bd14b778be6ae10557b0fd9d11
kenji hajiro <mademoiselle3824@gmail.com>
2021-12-13 11:23:58 +0900

    ログイン機能まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :pictures do
     collection do
       post :confirm
     end
   end
   resources :users do
-
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:new, :create, :destroy]
 end
