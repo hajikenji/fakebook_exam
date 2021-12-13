d68caf41ba7fb8c3081082c78d97b7e7d7b29af5
kenji hajiro <mademoiselle3824@gmail.com>
2021-12-14 00:21:42 +0900

    sqliteからpqに変更

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.0.1'
 gem 'rails', '~> 6.0.3'
+gem 'pg', '>= 0.18', '< 2.0'
 
 gem 'puma', '~> 4.1'
 gem 'sass-rails', '>= 6'
@@ -23,7 +24,7 @@ group :development do
   gem 'listen', '~> 3.2'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
-  gem 'sqlite3', '~> 1.4'
+  # gem 'sqlite3', '~> 1.4'
 end
 
 group :test do
--- db/schema.rb
+++ db/schema.rb
@@ -12,21 +12,24 @@
 
 ActiveRecord::Schema.define(version: 2021_12_13_023429) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pictures", force: :cascade do |t|
     t.text "image"
     t.string "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_pictures_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "user_name"
     t.string "email"
     t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
